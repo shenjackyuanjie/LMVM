07dc410b96035e83219a7d5560eb2c2877dea06e
shenjackyuanjie <3695888@qq.com>
2025-08-13 22:57:25 +0800

    进行一个代码正经化

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-pub(crate) mod Interpreter;
+pub(crate) mod interpreter;
 
-use crate::Interpreter::VM;
-use Interpreter::Runner;
+use crate::interpreter::vm;
+use interpreter::Runner;
 use std::{env, fs::File, io::Read};
 
-fn main() -> Result<(), std::io::Error> {
+fn main() -> anyhow::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         println!("Usage: {} <file>", args[0]);
@@ -13,9 +13,9 @@ fn main() -> Result<(), std::io::Error> {
     let mut file = File::open(args[1].clone())?;
 
     let register: Vec<i64> = vec![0; 10];
-    let heap: Vec<VM::Types> = vec![];
+    let heap: Vec<vm::Types> = vec![];
     let mut bytes: Vec<u8> = Vec::new();
-    (&mut file).read_to_end(&mut bytes)?;
+    file.read_to_end(&mut bytes)?;
 
     let runner = Runner {
         key: 0,
